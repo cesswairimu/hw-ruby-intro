6e8debd35f1facc3a894e9076313e685054382da
Cess <cessmbuguar@gmail.com>
2018-08-19 00:36:30 +0300

    part2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -30,15 +30,21 @@ end
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return false if s.chars.empty?
+  # s !~ (/^[^AEIOUaeiou]/)
+  !!(s[0] =~ /[bcdfghjklmnprstvwxyz]+/i)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if (s.to_s =~ /\b[01]+\b/ && s.to_i % 4 == 0)
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
